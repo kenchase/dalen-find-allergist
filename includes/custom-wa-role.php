c580c348d09a5398fff9b8478445579b09dc3c37
Ken Chase <kenchase@kenchase.com>
2025-10-03 15:22:10 -0400

    update redirect in custom-wa-role.php

--- includes/custom-wa-role.php
+++ includes/custom-wa-role.php
@@ -82,7 +82,7 @@ function fa_redirect_wa_users_from_admin()
         foreach ((array) $user->roles as $role) {
             if (strpos($role, 'wa_') === 0) {
                 // Get the edit profile page slug from plugin settings
-                $edit_profile_slug = get_option('fa_edit_profile_page_slug', 'my-account-wa');
+                $edit_profile_slug = get_option('fa_edit_profile_page_slug', 'my-account');
 
                 // Redirect to the front-end edit page
                 wp_redirect(home_url('/' . $edit_profile_slug . '/'));
